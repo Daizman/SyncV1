0c09ce5986c96349d34556ba21b79935f7ece8c2
Daizman <42967651+Daizman@users.noreply.github.com>
2021-03-12 22:31:20 +0500

    Сделал классы публичными

--- ToolsLib/SockCons.cs
+++ ToolsLib/SockCons.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ToolsLib
 {
-    class SockCons
+    public class SockCons
     {
     }
 }
--- ToolsLib/SockProd.cs
+++ ToolsLib/SockProd.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ToolsLib
 {
-    class SockProd
+    public class SockProd
     {
     }
 }
