66242af79bba2348d71f21cface245601d33daba
Daizman <42967651+Daizman@users.noreply.github.com>
2021-03-12 22:40:04 +0500

    Сделал заглушки для восстановления пользователя при открытии приложения

--- SyncV1/MainForm.cs
+++ SyncV1/MainForm.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ToolsLib.UserClasses;
 
 namespace SyncV1
 {
     public partial class MainForm : Form
     {
+        private User _user;
         public MainForm()
         {
             InitializeComponent();
@@ -23,9 +26,28 @@ namespace SyncV1
             AllowToDir.Enabled = true;
         }
 
-        private bool CheckUserInfo()
+        private void RestoreUser()
         {
-            return false;
+
+        }
+
+        private void GenerateUser()
+        { 
+            
+        }
+
+        private string GetUserBackupFile()
+        {
+            var allDirFiles = Directory.GetFiles(Environment.CurrentDirectory);
+            var fileExtenTemplate = UserSet.Default.UserDataExt;
+            foreach (var file in allDirFiles)
+            {
+                if (file.Contains(fileExtenTemplate))
+                {
+                    return file;
+                }
+            }
+            return "";
         }
     }
 }
